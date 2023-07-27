16d31d290a3957540ea4463f902d2092a7bc7eba
Jogan Daniel A <jogan.d@tvsnext.com>
2023-07-27 10:32:19 +0530

    [jogan] Add : Folder structure created

---
+++ DataStructuresAlgorithm/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace DataStructuresAlgorithm
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Data Structures program");
+        }
+    }
+}
